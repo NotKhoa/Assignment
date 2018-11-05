ed4026edef958f747600dbcfa1805b8727644905
Khoa Le <khoa.nicky@gmail.com>
2018-11-05 15:43:56 +1100

    Redid start_menu, added error handling

--- ledconfigure.sh
+++ ledconfigure.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
 
 start_menu() {
-
-	counter=0
+	local array=()
+	local counter=0
 	printf "Welcome to Led_Konfigurator\n"
 	printf "===========================\n"
 	
 	for i in /sys/class/led/* ; do
 		if [ -d "$i" ]; then
+			array[$counter]=${i} 
 			((counter++))
-			echo $counter. $(basename "$i")
+			echo $counter. "$(basename "$i")"
 		fi
 	done
 	((counter++))
 	echo $counter. "Quit"
-	total=$counter
-	start_input
+	start_input_menu "${array[@]}" 
 }
 
-start_input {
-	
-	counter=0
-	printf "Please enter a number (1-$total) for the led to configure or quit: "
-	read input
-	if [ $input -eq $total ]; then
-		echo "exit"
-		exit 0
-	fi
-
-	for i in /sys/class/leds/* ; do
-		if [ -d "$i" ]; then
-			((counter++))
-			if [ $counter -eq $input ]; then
-				directory=$(basename "$i")
-				echo $directory
-				break
-			fi
+start_menu_input() {
+	local array=("$@")
+	local total=$((${#array[@]} + 1))
+	printf "Please enter a number (1-%d) for the led to configure or quit: " "$total"
+	read -r input
+	if [ "$input" -eq "$input" ] 2>/dev/null; then	# Check user input is an integer
+		if [ "$input" -eq "$total" ]; then	# if user wants to quit
+			exit 0
+		elif [[ "$input" -lt "$total" && "$input" -gt 0 ]]; then	# User input is correct
+			directory=${array[$input-1]}	# Stores directory into variable
+			cd "$directory" || 2>/dev/null	# Move to input directory or move to null error
+			led_menu "$(basename "$directory")"
+		else
+			echo "Invalid input.. Returning to menu"
+			start_menu
 		fi
-	done
-	cd /sys/class/leds/$directory
-
+	else
+		echo "Invalid input.. Returning to menu"
+		start_menu
+	fi
 }
+
+
