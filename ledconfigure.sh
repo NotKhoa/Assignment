fc8fa1a34c1b4eee30511d8c3bb72332035e9817
Khoa Le <khoa.nicky@gmail.com>
2018-11-05 16:05:54 +1100

    Debugging and added led_menu

--- ledconfigure.sh
+++ ledconfigure.sh
@@ -6,7 +6,7 @@ start_menu() {
 	printf "Welcome to Led_Konfigurator\n"
 	printf "===========================\n"
 	
-	for i in /sys/class/led/* ; do
+	for i in /sys/class/leds/* ; do
 		if [ -d "$i" ]; then
 			array[$counter]=${i} 
 			((counter++))
@@ -15,7 +15,7 @@ start_menu() {
 	done
 	((counter++))
 	echo $counter. "Quit"
-	start_input_menu "${array[@]}" 
+	start_menu_input "${array[@]}" 
 }
 
 start_menu_input() {
@@ -40,4 +40,51 @@ start_menu_input() {
 	fi
 }
 
+led_menu() {
+	local name=$1
+	echo "$name"
+	echo "=============="
+	echo "What would you like to do with this led?"
+	echo "1. Turn on"
+	echo "2. Turn off"
+	echo "3. Associate with a system event"
+	echo "4. Associate with the performance of a process"
+	echo "5. Stop association with a process' performance"
+	echo "6. Quit to main menu"
+	led_menu_input "$name"
+	
+}
 
+led_menu_input() {
+	name=$1
+	printf "Please enter a number (1-6) for your choice: "
+	read -r input
+	case $input in
+		1)
+			echo "Turning on..."
+			echo "1">brightness
+			;;
+		2)
+			echo "Turning off..."
+			echo "0">brightness
+			;;
+		3)
+			echo "Associate with a system event"
+			;;
+		4)
+			echo "Associate with performance of process"
+			;;
+		5)
+			echo "Stopping association"
+			;;
+		6)
+			start_menu
+			;;
+		*)
+			echo "Invalid Input..."
+			led_menu $name
+			;;
+	esac
+}
+
+start_menu
